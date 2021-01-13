56dcf71ff7cb54faeb1e7c8dc39639b94c7c219e
Gabriel Mares <43484502+gabrielmares@users.noreply.github.com>
2021-01-13 12:29:51 -0700

    componente principal del proyecto, se cargan el resto de componentes sobre este

--- src/pages/Main.js
+++ src/pages/Main.js
@@ -2,7 +2,7 @@ import { useContext } from 'react'
 import Header from '../components/Header'
 import Sidebar from '../components/Sidebar'
 import Calendar from '../components/Calendar'
-import { Col } from 'reactstrap'
+import { Col, Row } from 'reactstrap'
 import { sessionContext } from '../provider/contextGlobal'
 
 
@@ -11,20 +11,24 @@ const MainComponent = () => {
     const { sidebar } = useContext(sessionContext)
 
     return (
-        <>
+        <div className="mainCss">
             <Header />
-            <div className="container row col-12">
-                {!sidebar && (<Sidebar />)}
-                <Col lg={!sidebar ? (8) : (10)} >
-                    <Calendar />
-                </Col>
-                <Col>
-                    Actividades de hoy
-                </Col>
-            </div>
-        </>
+            <Col lg={12}>
+                <Row>
+                    {!sidebar && (<Sidebar />)}
+                    <Col lg={!sidebar ? (8) : (10)}  >
+                        <Row>
+                            <Calendar />
+                        </Row>
 
+                    </Col>
+                    <Col className="sidebarcss" lg={2}>
+                        Actividades de hoy
+                    </Col>
+                </Row>
 
+            </Col>
+        </div>
     );
 }
 
