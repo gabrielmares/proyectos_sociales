dc33027b7d8c5e3fd067560899b586f29d0a789f
Gabriel Mares <43484502+gabrielmares@users.noreply.github.com>
2021-01-29 10:38:45 -0700

    reconfiguracion de componente, colores y distribucion de elementos

--- src/components/privates/Header.js
+++ src/components/privates/Header.js
@@ -1,13 +1,10 @@
 import { useState } from 'react'
 import {
     Nav,
-    Dropdown,
     DropdownMenu,
     DropdownItem,
     DropdownToggle,
-    NavbarText,
     NavItem,
-    Button,
     Navbar,
     NavbarBrand,
     NavbarToggler,
@@ -16,44 +13,35 @@ import {
     UncontrolledDropdown
 } from 'reactstrap'
 
+import { logOut } from '../../firebase/firebase'
+import { useHistory } from 'react-router-dom'
+
 
 const HeaderComponent = () => {
 
+    const history = useHistory();
     const [dropdownOpen, setDropdownOpen] = useState(false);
 
     const toggle = () => setDropdownOpen(!dropdownOpen);
+
+    const closeSession = () => {
+        logOut()
+        history.push('/');
+    }
     return (
         <>
-            {/* <Nav className="bg-primary justify-content-between text-white">
-                <Dropdown isOpen={dropdownOpen} toggle={toggle} className="mt-2">
-                    <DropdownToggle nav caret className="float-left mr-auto text-white">
-                        Reportes
-                </DropdownToggle>
-                    <DropdownMenu>
-                        <DropdownItem >Mensual</DropdownItem>
-                        <DropdownItem >Anual</DropdownItem>
-                        <DropdownItem>Por Proyecto</DropdownItem>
-                    </DropdownMenu>
-                </Dropdown>
-                <NavbarText className="my-auto">
-                    <h2><b>Proyectos Sociales</b></h2>
-                </NavbarText>
-                <NavItem className="p-2">
-                    <Button color="warning">Cerrar Sesion</Button>
-                </NavItem>
 
-            </Nav> */}
             <div>
                 <Navbar className="headercss" expand="md">
                     <NavbarBrand className="text-white mt-2"><h2><strong>Responsabilidad social</strong></h2></NavbarBrand>
                     <NavbarToggler onClick={toggle} />
                     <Collapse isOpen={dropdownOpen} navbar className="mt-4">
-                        <Nav className="ml-auto text-white" navbar>                           
+                        <Nav className="ml-auto text-white" navbar>
                             <UncontrolledDropdown nav inNavbar className="text-white">
                                 <DropdownToggle nav caret className="float-left text-white">
                                     Reportes
                                 </DropdownToggle>
-                                <DropdownMenu>
+                                <DropdownMenu style={{ height: 'fit-content', overflowY: 'hidden' }}>
                                     <DropdownItem >Mensual</DropdownItem>
                                     <DropdownItem >Anual</DropdownItem>
                                     <DropdownItem>Por Proyecto</DropdownItem>
@@ -62,7 +50,11 @@ const HeaderComponent = () => {
                             <NavItem>
                                 <NavLink href="#" className="text-white ml-2 mr-2">Administracion</NavLink>
                             </NavItem>
-                            <NavLink href="#" className="text-white">Cerrar sesion</NavLink>
+                            <NavLink
+                                className="text-white"
+                                style={{ cursor: 'pointer' }}
+                                onClick={closeSession}
+                            >Cerrar sesion</NavLink>
                         </Nav>
                     </Collapse>
 
