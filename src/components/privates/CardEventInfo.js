ca3c6f864c93582b9c1883054cf14a30fb62553a
Gabriel Mares <43484502+gabrielmares@users.noreply.github.com>
2021-02-01 21:09:45 -0700

    sidebar de eventos del dia y eventos pendientes por cerrar

---
+++ src/components/privates/CardEventInfo.js
@@ -0,0 +1,118 @@
+import { useContext, useState } from 'react'
+import { Card, CardHeader, CardBody, CardFooter, Row } from 'reactstrap'
+import { sessionContext } from '../../provider/contextGlobal'
+import moment from 'moment'
+
+const CardComponent = ({ event }) => {
+// eslint-disable-next-line 
+    const [localEvent, setLocalEvent] = useState(event)
+
+    const { title,
+        lugar,
+        responsable,
+        start,
+        end,
+        tipoEvento, } = localEvent
+
+
+    const {
+        setGenerales,
+        setProjectInfo,
+        setOnSite,
+        setDateEvent,
+        setperiod,
+        setSidebar,
+        setResetDropdown,
+        setVirtualEvent,
+        setActive,
+        setSelection,
+        setImpacto
+    } = useContext(sessionContext)
+
+    // formatear la fecha en modo DD/MM/DDDD
+    const parseDate = (date1) => {
+        const date = new Date(date1)
+        const datetime = date.getDate() + "/" + (date.getMonth() + 1) + "/" + date.getFullYear() + " " + date.getHours() + ":" + date.getMinutes()
+        return datetime;
+    }
+
+
+    const handleEditEvent = (e) => {
+        setActive(!moment(e.end.split(" ")[0]).isBefore(moment(), 'day'))
+        const {
+            title,
+            responsable,
+            start,
+            end,
+            lineasIntervencion,
+            impacto,
+            objetivo,
+            tema,
+            tipoEvento,
+            listaDeComunidadesAsignadas
+        } = e
+        setImpacto(impacto);
+        setGenerales({
+            objetivo,
+            title,
+            responsable,
+            lineasIntervencion,
+            tipoEvento,
+        })
+        setSidebar(false)
+        console.log(localEvent)
+        if (parseInt(tipoEvento) === 1) {
+            return setProjectInfo({
+                listaDeComunidadesAsignadas,
+                fechaInicio: start.replace(' ', "T"),
+                fechaFin: end.replace(' ', "T"),
+                tema
+            })
+        }
+        setResetDropdown(false);
+        if (parseInt(tipoEvento) === 2) {
+            return updateEvent(localEvent)
+        }
+        setResetDropdown(true);
+
+    }
+
+    const updateEvent = (evento) => {
+        const { period, lugar, invitados, start, end, selection } = evento
+        setOnSite(lugar || "");
+        setperiod(period || "");
+        setSelection(selection || 0);
+        setVirtualEvent(invitados || "");
+        setDateEvent({
+            start: start.replace(' ', 'T'),
+            end: end.replace(' ', 'T')
+        });
+    }
+
+
+    return (
+        <Card onDoubleClick={e => handleEditEvent(localEvent)} style={{ cursor: 'pointer' }}>
+            <CardHeader>
+                <Row>
+                    <span className="text-left mr-auto " style={{ fontSize: '14px' }}><strong>{title}</strong></span>
+                    <span style={{ fontSize: '11px', marginRight: '2px', textTransform: 'uppercase' }}>
+                        {/* tipo de evento, si es proyecto aparecen COMUNIDADES, si no, lo que se almaceno en la coleccion */}
+                        {(parseInt(tipoEvento) === 1) ? ('COMUNIDADES') : (lugar)}
+                    </span>
+                </Row>
+            </CardHeader>
+            <CardBody>
+                <span>{responsable}</span>
+            </CardBody>
+            <CardFooter style={{ fontSize: '11px' }}>
+                <Row>
+                    {/* fecha de inicio del evento y fecha final */}
+                    <span className="text-left mr-auto">{parseDate(start)}</span>
+                    <span className="text-right">{parseDate(end)}</span>
+                </Row>
+            </CardFooter>
+        </Card>
+    );
+}
+
+export default CardComponent;
---
+++ src/components/privates/SidebarEventsInfo.js
@@ -0,0 +1,57 @@
+import { useContext, useState } from 'react'
+import { sessionContext } from '../../provider/contextGlobal'
+import CardComponent from './CardEventInfo'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import moment from 'moment'
+import { faBell } from '@fortawesome/free-solid-svg-icons'
+
+
+const SidebarEventsComponent = () => {
+
+    const { coleccion } = useContext(sessionContext)
+    const [toggle, setToggle] = useState(false)
+    let newArray = [], outDatePending = [];
+    // mapeo de eventos del dia que se desplegaran en el sidebarEvents
+    coleccion.map(event => {
+        // cuando el evento es actividad de un dia, se valida que el evento sea solo de este dia
+        if (moment(event.start.split(' ')[0]).isSame(Date.now(), 'day') && parseInt(event.tipoEvento) === 2) {
+            newArray.push(event)
+        }
+        if (parseInt(event.tipoEvento) === 1 && (moment(event.start.split(' ')[0]).isSame(Date.now(), 'day') || (moment().isBetween(event.start.split(' ')[0], event.end.split(' ')[0])))) {
+            newArray.push(event)
+        }
+        return newArray
+    })
+
+    // revision de objetos fuera de fecha que aun no tienen el numero de personas impactadas
+    outDatePending = coleccion.filter(event => (moment(event.end.split(' ')[0]).isBefore(moment(), 'day') && parseInt(event.impacto) === 0))
+
+    return (
+        <>
+            <div className="row pt-4 pb-2">
+                {(!toggle || outDatePending.length === 0) ? ((newArray.length > 0) ? (<h4 className="ml-auto" >Actividades para hoy</h4>)
+                    : (<h4 className="ml-auto">Sin programacion para hoy</h4>))
+                    : (<h4 className="ml-auto">Eventos pendientes de cierre</h4>)
+                }
+                <i
+                    title={` ${(outDatePending.length > 0) ? ('Hay eventos pendientes de cerrar') : ('Sin cierres pendientes')}`}
+                    className="ml-auto"
+                    onClick={() => (outDatePending.length > 0) && setToggle(!toggle)}
+                >
+                    <FontAwesomeIcon
+                        icon={faBell}
+                        className={`${(outDatePending.length > 0) && ('iconBell')}  ml-auto mr-4`}
+                        size='2x'
+                    />
+                </i>
+            </div>
+
+            {((!toggle && newArray.length > 0) || outDatePending.length === 0) && (newArray.map((thisEvent, index) => (<CardComponent key={index} event={thisEvent} />)))}
+            {(toggle && outDatePending.length > 0) && (outDatePending.map((outdate, index) => (<CardComponent key={index} event={outdate} />)))}
+
+
+        </>
+    );
+}
+
+export default SidebarEventsComponent;
