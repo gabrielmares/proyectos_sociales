e5bc8f9c5a4021553cddddf7ec4122a7d4a627a0
Gabriel Mares <43484502+gabrielmares@users.noreply.github.com>
2021-02-01 21:17:28 -0700

    cambio de estructura de pantalla principal

--- src/pages/Main.js
+++ src/pages/Main.js
@@ -4,28 +4,32 @@ import Sidebar from '../components/privates/Sidebar'
 import Calendar from '../components/privates/Calendar'
 import { Col, Row } from 'reactstrap'
 import { sessionContext } from '../provider/contextGlobal'
+import SidebarEventsComponent from '../components/privates/SidebarEventsInfo'
+// import GlobalContext from '../provider/contextGlobal'
 
 
-const MainComponent = () => {
+const MainComponent = (props) => {
 
     const { sidebar } = useContext(sessionContext)
 
     return (
-        <div className="mainCss">
-            <Header />
-            <Col lg={12}>
-                <Row >
-                    {!sidebar && (<Sidebar />)}
-                    <Col lg={!sidebar ? (8) : (10)}  >
-                        <Calendar />
+        // <GlobalContext>
+            <div className="mainCss">
+                <Header {...props} />
+                <Col lg={12}>
+                    <Row >
+                        {!sidebar && (<Col lg={3}><Sidebar  {...props} /></Col>)}   
+                        <Col lg={!sidebar ? (6) : (9)}  >
+                            <Calendar {...props} />
 
-                    </Col>
-                    <Col className="sidebarcss" lg={!sidebar && (2)}>
-                        <h4 className="text-center pt-4">Actividades para hoy</h4>
-                    </Col>
-                </Row>
-            </Col>
-        </div>
+                        </Col>
+                        <Col className="sidebarcss" lg={3}>
+                            <SidebarEventsComponent  {...props} />
+                        </Col>
+                    </Row>
+                </Col>
+            </div>
+        // </GlobalContext>
     );
 }
 
